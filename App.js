f03409b916a9e6cd965132814a24dac11729adce
Shivanshi Tiwari <shivanshitiwari381@gmail.com>
2024-04-05 17:55:50 +0530

    updated

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from './src/components/Tabs'
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [location,setLocation]=useState(null);
+  const[error,setError]=useState(null)
 
   // Set loading to true as when the app launches it will see the activity indicator
   if (loading) {
